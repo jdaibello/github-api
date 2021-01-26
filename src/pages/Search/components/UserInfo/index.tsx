9260627b1981fef29acb71c39c982ae72f8cf54c
João Pedro <jpedrodaibello1999@outlook.com>
2021-01-25 21:46:02 -0300

    Showing user details from Github API

--- src/pages/Search/components/UserInfo/index.tsx
+++ src/pages/Search/components/UserInfo/index.tsx
@@ -1,50 +1,74 @@
 import React from 'react';
+import { User } from '../../../../types/user';
+import ImageLoader from '../Loaders/ImageLoader';
+import InfoLoader from '../Loaders/InfoLoader';
 import './styles.css';
 
-function UserInfo() {
+type Props = {
+  user: User;
+  isLoading: boolean;
+}
+
+function UserInfo({ user, isLoading }: Props) {
   return (
     <div className="user-info-container">
       <div className="user-info">
         <div className="user-info-avatar-content">
-          <img
-            className="user-info-avatar"
-            src="https://avatars2.githubusercontent.com/u/13897257?v=4"
-            alt="Avatar do usuário"
-          />
-          <button className="btn btn-primary">
-            Ver perfil
-          </button>
+          {isLoading ? (
+            <ImageLoader />
+          ) : (
+              <>
+                <img
+                  className="user-info-avatar"
+                  src={user.avatar_url}
+                  alt="Avatar do usuário"
+                />
+                <a
+                  href={`https://github.com/${user.login}`}
+                  className="btn btn-primary"
+                  target="_new"
+                >
+                  Ver perfil
+                </a>
+              </>
+            )}
         </div>
         <div className="user-info-details">
-          <div className="badges-content">
-            <span className="badge-item user-info-item">
-              Repositórios públicos: 
-            </span>
-            <span className="badge-item user-info-item">
-              Seguidores: 
-            </span>
-            <span className="badge-item user-info-item">
-              Seguindo: 
-            </span>
-          </div>
-          <div className="user-info-content user-info-item">
-            <h3 className="user-info-title">Informações</h3>
-            <div className="user-info-details-item user-info-item">
-              <strong>Empresa:</strong> 
-            </div>
-            <div className="user-info-details-item user-info-item">
-              <strong>Website/Blog:</strong> 
-            </div>
-            <div className="user-info-details-item user-info-item">
-              <strong>Localidade:</strong> 
-            </div>
-            <div className="user-info-details-item user-info-item">
-              <strong>Membro desde:</strong> 
-            </div>
-          </div>
+          {isLoading ? (
+            <InfoLoader />
+          ) : (
+              <>
+                <div className="badges-content">
+                  <span className="badge-item user-info-item">
+                    Repositórios públicos: {user.public_repos}
+                  </span>
+                  <span className="badge-item user-info-item">
+                    Seguidores: {user.followers}
+                  </span>
+                  <span className="badge-item user-info-item">
+                    Seguindo: {user.following}
+                  </span>
+                </div>
+                <div className="user-info-content user-info-item">
+                  <h3 className="user-info-title">Informações</h3>
+                  <div className="user-info-details-item user-info-item">
+                    <strong>Empresa:</strong> {user.company}
+                  </div>
+                  <div className="user-info-details-item user-info-item">
+                    <strong>Website/Blog:</strong> {user.blog}
+                  </div>
+                  <div className="user-info-details-item user-info-item">
+                    <strong>Localidade:</strong> {user.location}
+                  </div>
+                  <div className="user-info-details-item user-info-item">
+                    <strong>Membro desde:</strong> {user.created_at}
+                  </div>
+                </div>
+              </>
+            )}
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { User } from '../../types/user';
+import { makeRequest } from '../../utils/request';
 import Form from './components/Form';
 import UserInfo from './components/UserInfo';
 import './styles.css';
 
 function Search() {
+  const [userInfo, setUserInfo] = useState<User>();
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSearch = (search: string) => {
-    console.log(search);
+    makeRequest({ url: `/${search}` })
+      .then(response => {
+        setUserInfo(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }
 
   return (
     <div className="search-container">
       <Form onSearch={handleSearch} />
-      <UserInfo />
+      {
+        userInfo && (
+          <UserInfo user={userInfo} isLoading={false} />
+        )
+      }
     </div>
   );
 }
